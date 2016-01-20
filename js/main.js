565d1df91e8a8d3e81ff4bd4465bc2f7abc71b40
elzaVlasova <ver.26.vvv@gmail.com>
2016-01-20 14:32:24 +0200

    First JS changes

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 112 on 20.01.2016.
+ */
+
+
+$(function(){
+//This	function	will	be	called
+//	after	the	DOM	was	loaded	and	ready
+//Here	we	can	place	all	jQuery	Code
+    $('button').click(function(){
+        $(this).css("font-size", "25px")
+    });
+});
